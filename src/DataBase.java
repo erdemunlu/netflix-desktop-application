cc5a5e4a08d1aca767a4ba602616714065572aed
Erdem Unlu <erdemunlu17@gmail.com>
2020-06-11 16:05:40 +0300

    fix

--- src/DataBase.java
+++ src/DataBase.java
@@ -2,7 +2,7 @@
 public class DataBase {
     
     public static final String kullanici_adi = "root";///HER YERDEN ERİŞİLEBİLSİN AMA DEĞİŞTİRİLEMESİN(STATİC FİNAL)
-    public static final String parola ="a78YMr3dv";
+    public static final String parola ="password";
     
     public static final String db_ismi = "netflixdb";
 
