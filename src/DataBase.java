39a0783c910eefb2f8f4870d6274300a109cb9ed
erdemunlu <66748140+erdemunlu@users.noreply.github.com>
2020-06-11 19:39:55 +0300

    Update DataBase.java

--- src/DataBase.java
+++ src/DataBase.java
@@ -1,7 +1,7 @@
 
 public class DataBase {
     
-    public static final String kullanici_adi = "root";///HER YERDEN ERİŞİLEBİLSİN AMA DEĞİŞTİRİLEMESİN(STATİC FİNAL)
+    public static final String kullanici_adi = "root";
     public static final String parola ="password";
     
     public static final String db_ismi = "netflixdb";
